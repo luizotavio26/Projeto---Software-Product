e924f14f7a9b57442e62b049d32acea23809e942
JoaoPedro-SA <antunesjoaopedro3@gmail.com>
2025-10-22 18:15:13 -0300

    criação das rotas para os modelos test documentos

--- Controller/documentos_controller.py
+++ Controller/documentos_controller.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, request, jsonify
 from Model.documentos import documentos_model
+from Model.documentos.tests.test import download_pdf
+from Model.documentos.tests.test2 import *
+from Model.documentos.tests.test3 import *
 
 
 documentos = Blueprint('documentos', __name__)
@@ -7,5 +10,18 @@ documentos = Blueprint('documentos', __name__)
 @documentos.route("/test", methods=['GET'])
 def test():
     return jsonify({"message":"Documentos Blueprint funcionando!"}), 200
+
+@documentos.route("/test/<int:id>", methods=['GET'])
+def get_documento(id):
+    if id == 1: 
+        return download_pdf()
+    elif id == 2:
+        return gerar_relatorio()
+    elif id == 3:
+        return gerar_faturamento()
+    else:
+        return jsonify({"message": f"Documentos Blueprint funcionando! ID recebido: {id}"}), 200
+
+    
 
 
