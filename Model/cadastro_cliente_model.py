662c6e36529860c3c8921b282e58dc769304a37f
Ana Beatriz Silva Santos <ana1908beatrizsantos@gmail.com>
2025-11-11 09:15:42 -0300

    Feat: login com token gerado

--- Model/cadastro_cliente_model.py
+++ Model/cadastro_cliente_model.py
@@ -158,3 +158,30 @@ def deleteTodosClientes():
     db.session.commit()
     return {'message':"Usuários deletados com sucesso!"}
 
+def verifica_senha_email(dados):
+    #consultando o funcionario pelo email
+    cliente = Clientes.query.filter_by(email=dados["email"]).first()
+
+    #vendo se o email é valido
+    if cliente is None:
+        return {"message": "registro não encontrado, faça seu cadastro"}
+    else:
+        #vendo se senha está correta
+        if dados["senha"] != cliente.senha:
+            return {"message": "senha invalida"}
+
+        # se tudo estiver certo
+        else:
+            #Gerando o token
+            token = jwt.encode(
+            {"email": dados["email"], "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)},
+            SECRET_KEY,
+            algorithm="HS256"
+        )
+            
+            # retornando a mensagem de sucesso e o token
+           return (
+               {"message": "Login realizado com sucesso", 
+                    "token": token, 
+                    "success": True}
+           )
